1f87821624ed53d85dd0f744b2a95c18b22d345a
Ahmed3llam <ahmedmohamedallam230@gmail.com>
2021-12-09 18:35:27 +0200

    creat master temp

---
+++ src/components/Test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      {/* <Router>
+            <Navigation/>
+            <Switch>
+                <Route path="/" exact component={() => <Home/>}/>
+               <Route path="/about" exact component={() => <About/>}/>
+                <Route path="/contact" exact component={() => <Contact/>}/>
+            </Switch>
+            <Footer/>
+        </Router> */}
+    </div>
+  );
+}
+export default App;
